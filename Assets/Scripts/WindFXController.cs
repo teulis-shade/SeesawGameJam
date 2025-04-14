0f1fd9e4a125f217dfb41684270b0c9995f73b16
chernandez67 <chernandez67@gatech.edu>
2025-04-14 01:05:18 -0400

    Add wind controller

--- Assets/Scripts/WindFXController.cs
+++ Assets/Scripts/WindFXController.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WindFXController : MonoBehaviour
 {
+    [Range(0f, 2f)]
+    public float windStrength = 0f;
+    public List<AudioSource> windSources = new List<AudioSource>();
 
-    public AudioClip windSound;
+    void Update()
+    {
+        int lowerIndex = Mathf.FloorToInt(windStrength);
+        int upperIndex = Mathf.Min(lowerIndex + 1, windSources.Count - 1);
+        float t = windStrength - lowerIndex;
+        for (int i = 0; i < windSources.Count; i++)
+        {
+            if (i == lowerIndex)
+                windSources[i].volume = 1f - t;
+            else if (i == upperIndex)
+                windSources[i].volume = t;
+            else
+                windSources[i].volume = 0f;
+        }
+    }
 
     void Start()
     {
-        AudioSource audioSource = GetComponent<AudioSource>();
-        if (audioSource != null && windSound != null) {
-            audioSource.clip = windSound;
-            audioSource.Play();
+        foreach (var source in windSources) {
+            if (!source.isPlaying)
+                source.Play();
         }
     }
 

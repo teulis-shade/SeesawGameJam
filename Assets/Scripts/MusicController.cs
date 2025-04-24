93b821928d6c0c669a96149aeb0b741b0410ddf0
Teulis <tziegler30@gatech.edu>
2025-04-23 21:07:27 -0400

    music working

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -14,32 +14,22 @@ public class MusicController : MonoBehaviour
     public List<CharacterMusic> musicStorage;
 
     private AudioSource source;
-    private AudioSource characterSource;
 
     public void Initialize()
     {
         source = GetComponent<AudioSource>();
-        characterSource = gameObject.AddComponent<AudioSource>();
-        characterSource.loop = true;
-        characterSource.Pause();
     }
 
     public void ChangeMusic(PlayerScript.Character character)
     {
-        if (!characterSource.isPlaying)
-        {
-            source.Play();
-            source.time = 0;
-            characterSource.time = 0;
-        }
         foreach (CharacterMusic music in musicStorage)
         {
             if (music.character == character)
             {
-                float characterTime = characterSource.time;
-                characterSource.clip = music.music;
-                characterSource.Play();
-                characterSource.time = characterTime;
+                float characterTime = source.time;
+                source.clip = music.music;
+                source.Play();
+                source.time = characterTime;
                 return;
             }
         }

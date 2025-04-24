2605445f6f594ba880ecd70a370215728d0b94a5
Teulis <tziegler30@gatech.edu>
2025-04-24 17:06:47 -0400

    Journal added
    Function added to the window to add them to the journalCheck prefab

---
+++ Assets/Scripts/EditorFunctions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorFunctions
+{
+    [MenuItem("EditorFunctions/Copy FlyingObjects into Journal")]
+    public static void MakeJournalEntries()
+    {
+        GameController gc = GameObject.FindObjectOfType<GameController>();
+        JournalCheck journal = GameObject.FindObjectOfType<JournalCheck>();
+        foreach (GameController.FlyingObjectContainer obj in gc.flyingObjectInit)
+        {
+            journal.journalEntries.Add(new JournalCheck.JournalEntry(obj.obj.GetComponent<FlyingObject>().objectName, obj.obj.GetComponent<FlyingObject>().description, obj.obj.GetComponent<SpriteRenderer>().sprite));
+        }
+    }
+}
